61769130fc397e72e4446139f2dc2b54161189b5
Sinawo <sinaowmngxuma@gmail.com>
2023-02-16 03:49:32 -0800

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-/***** This is an Entry point 
- *main - Entry point
- *
- * ** And if this function works it should Return 0
+/**
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
