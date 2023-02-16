6a3ba6d82552a25b62ed0bd28d51d55a238ffa15
Sinawo <sinaowmngxuma@gmail.com>
2023-02-16 03:46:29 -0800

    printing using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+
+/***** This is an Entry point 
+ *main - Entry point
+ *
+ * ** And if this function works it should Return 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+
+}
