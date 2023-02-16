dc6c63bd4f692a9f792000cbc4d8fa5be4848b2f
Sinawo <sinaowmngxuma@gmail.com>
2023-02-16 06:15:14 -0800

    fixed betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,16 @@
  * Return: Always 0 (Success)
  */
 
- int main (void)
- {	char charSize;
+int main(void)
+{
+	char charSize;
 	
 	int intSize;
-	
+
 	long int longInt;
-	
+
 	long long longLongInt;
-	
+
 	float floatSize;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(charSize));
@@ -22,5 +23,5 @@
 	printf("Size of a long int: %d byte(s)\n", sizeof(longInt));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(longLongInt));
 	printf("Size of a float: %d byte(s)\n", sizeof(floatSize));
- 	return (0);
- }
+	return (0);
+}
