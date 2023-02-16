b5391ccb519e360c76920ca91c35866e4971ae1c
Sinawo <sinaowmngxuma@gmail.com>
2023-02-16 05:19:11 -0800

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
- 	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",32);
+        char[] quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, quote, strlen(quote));
 	return (1);
 
 }
