238e3d73cf93c28eecc1595a5ebce71d3d6b1ccd
Sinawo <sinaowmngxuma@gmail.com>
2023-02-17 07:57:26 -0800

    Print base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+		printf("%d", i++);
+	putchar('\n');
+	return (0);
+}
