68ea2ddedbc8ca3017ab645d9dfb43bf42c899d2
Sinawo <sinaowmngxuma@gmail.com>
2023-02-17 03:07:07 -0800

    This betty style is crap!

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <time.h> 
+#include <time.h>
 #include <stdlib.h>
 int main(void)
 {
@@ -7,12 +7,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) 
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if ( n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
-	else 
+	else
 		printf("%d is zero\n", n);
-	return (0);
-		
+	return (0);	
 }
