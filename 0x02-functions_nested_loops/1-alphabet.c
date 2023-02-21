8f79c49eeac71c719f12ca34cef03ebb57894137
Sinawo <sinaowmngxuma@gmail.com>
2023-02-21 08:19:50 -0800

    {betty style}

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,11 +10,11 @@
 void print_alphabet(void)
 {
 	char ch = 'a';
-
-        while (ch <= 'z')
-        {
-                _putchar(ch);
-                ch++;
+	
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
 	}
 	_putchar('\n');
 }
