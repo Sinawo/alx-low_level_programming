d401be25534b9a804716b7c68907487090d38f95
Sinawo <sinaowmngxuma@gmail.com>
2023-02-21 07:00:20 -0800

    _putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-#include <time.h>
+#include "main.h"
 #include <stdlib.h>
 /**
  * main - Entry point
  *
- * this functions print _putchar
+ * this functions print
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch = 'a';
-
-	while (ch <= 'z')
-	{
-		if (ch == 'e' || ch == 'q')
-			ch++;
-		putchar(ch);
-		ch++;
-	}
-	putchar('\n');
+	/* printf("File :%s\n", __FILE__ );
+	 * printf("Date :%s\n", __DATE__ );
+	 * printf("Time :%s\n", __TIME__ );
+	 * printf("Line :%d\n", __LINE__ );
+	 * printf("ANSI :%d\n", __STDC__ );
+	 */
+	_putchar('a');
+	_putchar(10);
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+/**
+ * _putchar function is customised function that prints char
+ * output
+ * Return: 1 or 0
+ */
+int _putchar(int ch);
