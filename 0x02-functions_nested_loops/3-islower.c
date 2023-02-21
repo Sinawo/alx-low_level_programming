bc5e59c16d642d749de15e4b0b6b3f82a3735ded
Sinawo <sinaowmngxuma@gmail.com>
2023-02-21 09:23:52 -0800

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_alphabet - function
+ *
+ * Description: This function prints alphabet in lowercase
+ *
+ * Return: aphabet std output
+ */
+int _islower(int c)
+{
+	if (islower(c))
+		return 1;
+	else
+		return 0;
+}
