f002052e769d64ce08a55a07c933d6a34e60e24b
Sinawo <sinaowmngxuma@gmail.com>
2023-02-24 13:08:50 -0800

    multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,10 +8,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void positive_or_negative(int n)
 {
-	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -20,5 +18,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-	return (0);
 }
