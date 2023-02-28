656585a2b498cd4317d2537239287f35ba605ac0
Sinawo <sinaowmngxuma@gmail.com>
2023-02-28 15:40:54 -0800

    delete 0x05 folder it was arepository

---
+++ Tests/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - function 
+ *
+ * Description: takes a pointer to an interger and sets its value to to 98
+ *
+ * Return: no return value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
