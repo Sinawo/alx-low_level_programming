373fb2ec94ed5436f4b394638bc874ef3134c528
Sinawo <sinaowmngxuma@gmail.com>
2023-02-28 19:33:49 -0800

    print array of numbers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "_putchar.c"
+/**
+ * print_array - prints string woth commas
+ * @a: array of integers
+ * @n: numbers
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i; 
+
+	for(i = 0; i < n; i++)
+	{
+		_putchar(a[i] + '0');	
+		if (i != n-1)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+		}
+
+	}
+	_putchar('\n');
+	
+}
