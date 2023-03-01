9d16befde04e69ddb56bc79edcc217eb6b08a10b
Sinawo <sinaowmngxuma@gmail.com>
2023-02-28 16:31:27 -0800

    fixed betty style

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,16 @@
 /**
  * _strlen - returns the length of the string
  * @s: string to return
+ * Return: length of string
  */
 int _strlen(char *s)
 {
 	int count;
 
-	count =0;
+	count = 0;
 
-	while (s[count] != '\0') count++;
+	while (s[count] != '\0')
+		count++;
 
-	return count;
+	return (count);
 }
