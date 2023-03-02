06bdccf362383a08ffaaaa957d3e4682a2299907
Sinawo <sinaowmngxuma@gmail.com>
2023-03-02 10:52:57 -0800

    betty style limmit 80

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,21 +3,22 @@
 /**
  * _strncat - concatenates(combines) two strings
  * @n: uses at most n bytes from @src
- * @src: This is the string to be appended. This should not overlap the destination.
- * @dest: This is pointer to the destination array, which should contain a C string,
- * and should be large enough to contain the concatenated resulting string.
+ * @src: This is the string to be appended. This should not overlap the
+ * destination.
+ * @dest: This is pointer to the destination array, which should contain
+ * a C string, and should be large enough to contain the concatenated
+ * resulting string.
  * Return: This function returns a pointer to the resulting string
  */
 char *_strncat(char *dest, char *src, int n)
 {	
-	int char2ExtractIndex = 0;
-	int index4dest;
+	int count1;
+
+	int count2;
 	
-	for (index4dest = _strlen(dest); char2ExtractIndex < n && char2ExtractIndex < _strlen(src); index4dest++)
+	for (count2 = _strlen(dest); count1 < n && count1 < _strlen(src); count2++)
 	{
-		dest[index4dest] = src[char2ExtractIndex++];
+		dest[count2] = src[count1++]
 	}
 	return (dest);
-
-
 }
