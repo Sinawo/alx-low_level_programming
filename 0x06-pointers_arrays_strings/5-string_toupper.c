998b34ee0c7e31a6eb798f48f19c1951ae229443
Sinawo <sinaowmngxuma@gmail.com>
2023-03-02 15:47:51 -0800

    to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "stringlength.c"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * @str: string to change its elements
+ *
+ * Return: This function returns uppercase letters
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for(i = 0; i < _strlen(str); i++)
+	{
+		if(str[i] >='a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+	}
+	return (str);
+}
