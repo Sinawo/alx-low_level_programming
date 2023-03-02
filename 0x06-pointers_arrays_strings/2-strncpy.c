da61332493a47016ceb726096d90513e8883213b
Sinawo <sinaowmngxuma@gmail.com>
2023-03-02 13:29:40 -0800

    done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,24 @@
-#include <stdio.h>
 #include "stringlength.c"
 /**
- * _strncat - concatenates(combines) two strings
+ * _strncpy - copies a string
  *
- * @n: The number of characters to be copied from source.
+ * @n: The number of characters to be copied from source
  * @src: This is the string to be copied.
- * @dest: This is pointer to the destination array where the is to be copied.
+ * @dest: This is pointer to the destination array ehere the content
+ * is to be copied
  *
- * Return: This function returns a pointer to the copied string.
+ * Return: This function returns the pointer to the copied string
  */
 char *_strncpy(char *dest, char *src, int n)
-{	
+{
 	int indx1;
 
-	int indx2;
-	
-	index1 = 0;
-	for (indx2 = _strlen(dest); (indx1 < n && indx1 < _strlen(src));)
+	for (indx1 = 0; (indx1 < n && src[indx1] != '\0'); indx1++ )
 	{
-		dest[indx2] = src[indx1++];
-		indx2++;
+		dest[indx1] = src[indx1];
 	}
+	while (indx1 < n)
+			dest[indx1++] = '\0';
+
 	return (dest);
 }
