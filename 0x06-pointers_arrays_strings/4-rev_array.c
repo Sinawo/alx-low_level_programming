83190da85780cc342f865356c6bded9be596a93b
Sinawo <sinaowmngxuma@gmail.com>
2023-03-02 15:32:22 -0800

    betty style fixed

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,11 @@
 #include "stringlength.c"
 /**
- * _strcmp - compares the strings
+ * reverse_array - reverse array elements
  *
- * @s1: First string
- * @s2: Second string.
+ * @a: array to change its elements
+ * @n: number of elements.
  *
- * Return: This function returns -/+ number or 0 resulting from comarison.
+ * Return: This function returns reversed array elements
  */
 void reverse_array(int *a, int n)
 {
@@ -14,7 +14,7 @@ void reverse_array(int *a, int n)
 
 	for(i = 0; i < n / 2; i++)
 	{
-        	tmp = a[i];
+		tmp = a[i];
 		a[i] = a[n - i - 1];
 		a[n - i - 1] = tmp;
 	}
