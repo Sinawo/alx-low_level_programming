8c34abe2e38ef474d2e5d0df908c13a5c0dbf1c5
Sinawo <sinaowmngxuma@gmail.com>
2023-03-03 10:03:13 -0800

    betty styles

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,10 +11,10 @@
 char *cap_string(char *str)
 {
 	int i;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ' || str[i] == ',' || str[i] == '!' || str[i] == '.' )
+		if (str[i] == ' ' || str[i] == ',' || str[i] == '!' || str[i] == '.')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] = str[i + 1] - 32;
@@ -29,12 +29,12 @@ char *cap_string(char *str)
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] = str[i + 1] - 32;
-                }
+		}
 		else if (str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-                {
-                        if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                                str[i + 1] = str[i + 1] - 32;
-                }
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] = str[i + 1] - 32;
+		}
 	}
 	return (str);
 }
