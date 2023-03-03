47570af3257be13ead526ab9e64ce6829b94f4e5
Sinawo <sinaowmngxuma@gmail.com>
2023-03-02 16:00:56 -0800

    betty style on reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,10 +12,10 @@ void reverse_array(int *a, int n)
 	int i;
 	int tmp;
 
-	for(i = 0; i < n / 2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		tmp = a[i];
-		a[i] = a[n-i-1];
-		a[n-i-1] = tmp;
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
 	}
 }
