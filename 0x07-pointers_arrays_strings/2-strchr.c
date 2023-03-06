163a5960b3b1f1fea3fd4865383adc0bd259bc41
Sinawo <sinaowmngxuma@gmail.com>
2023-03-06 07:14:46 -0800

    return the address(pointer) of located c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "stringlength.c"
+#include <stddef.h>
+#include <stdbool.h>
+/**
+ * strchr - locates a character in a string
+ * @s: string passed
+ * @c: character to locate in the string s
+ * Return: a pointer(array or string) to the first occurrence of the character c in the
+ * string s, or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	int len = _strlen(s);
+
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return NULL;
+}
