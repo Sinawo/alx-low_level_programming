26d73e2d559412de8dd869b6d8bdd6154c3dae47
Sinawo <sinaowmngxuma@gmail.com>
2023-03-06 13:03:30 -0800

    update

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include <stdbool.h>
+/**
+ * _strspn - Length of initial segment matching
+ * @s: string to be scanned
+ * @accept: string containing the list of characters to match in s
+ *
+ * Return: returns the number of characters in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+	int i, x;  
+	unsigned int count = 0;
+
+	for (i = 0, x = 0; (s[i] != '\0'); i++)
+	{
+		for (; s[x] != '\0'; x++)
+		{
+			if (s[i] == accept[x])
+			{
+				count++;
+				break;
+			}
+		}
+	}
+	return (count);
+}
