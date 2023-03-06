2f1d4dfd9e09d49a00186ec62f2a9ad8e034dd0c
Sinawo <sinaowmngxuma@gmail.com>
2023-03-06 13:06:59 -0800

    fixed betty and last test

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,10 +10,10 @@
 unsigned int _strspn(char *s, char *accept)
 {
 
-	int i, x;  
+	int i, x;
 	unsigned int count = 0;
 
-	for (i = 0, x = 0; (s[i] != '\0'); i++)
+	for (i = 0, x = 0; (s[i] != '\0') && (s[x] != '\0'); i++)
 	{
 		for (; s[x] != '\0'; x++)
 		{
