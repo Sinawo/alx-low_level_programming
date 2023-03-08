4252a826cfc6ffbb788ea73ed07224bebf3c795e
Sinawo <sinaowmngxuma@gmail.com>
2023-03-08 06:17:00 -0800

    string length betty style fixed

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,7 +9,7 @@ int _strlen_recursion(char *s)
 	int count = 0;
 
 	if (*s == '\0')
-		return count;
+		return (count);
 	count++;
-	return count + _strlen_recursion(s + 1);
+	return (count + _strlen_recursion(s + 1));
 }
