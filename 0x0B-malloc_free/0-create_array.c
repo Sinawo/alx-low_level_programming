7079f2aaa8f6c6375e4972500c8f4b7f63357a13
Sinawo <sinaowmngxuma@gmail.com>
2023-03-13 13:38:28 -0700

    fixed programs compilation errors

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	arr = malloc(size * sizeof(char));
 	if (arr == NULL)
-		return (NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		*(arr + i) = c;
 	return (arr);
