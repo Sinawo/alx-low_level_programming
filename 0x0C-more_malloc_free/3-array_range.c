5eb1455c7103879f2c0f98359cfbb08203810680
Sinawo <sinaowmngxuma@gmail.com>
2023-03-17 18:02:39 -0700

    creates array of ints from min to max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,23 +1,25 @@
 #include <stdlib.h>
 /**
- * *_calloc - eates an array of chars, and initializes it with
- * a specific char
- * @size: size of the array based in int types
- * @nmemb: char that initializes am array
+ * array_range - creates and array of integers
+ * @min: size of the array based in int types
+ * @max: char that initializes am array
  * Return: NULL if size = 0 otherwise pointer to the array
  */
 
-void *_calloc(unsigned int nmemb, unsigned int size)
+int *array_range(int min, int max)
 {
-	char *arr;
-	unsigned int i;
+	int *arr;
+	int i;
+	int size = max - min;
 
-	if (nmemb == 0 || size == 0)
+	if (min > max)
 		return (NULL);
-	arr = malloc(size * nmemb));
+	arr = malloc(sizeof(int) * (max - min));
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		*(arr + i) = 0;
+	for (i = 0; i < size; i++, min++)
+		*(arr + i) = min;
+
+
 	return (arr);
 }
