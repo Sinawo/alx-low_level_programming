5103d614e974cdbadc5211af6fca80e7eeb1129c
Sinawo <sinaowmngxuma@gmail.com>
2023-03-17 19:09:01 -0700

    used diff range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,22 +1,26 @@
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * array_range - creates and array of integers
- * @min: size of the array based in int types
- * @max: char that initializes am array
- * Return: NULL if size = 0 otherwise pointer to the array
+ * @min: minimum rage to print
+ * @max: max int to print
+ * Return: NULL if min > max or malloc fails otherwise pointer to the array
  */
 int *array_range(int min, int max)
 {
 	int *arr;
 	int i;
-	int size = max - min + 1;
+	int size = 0;
+	
+	for (i = min; i <= max; i++)
+		size++;
 
 	if (min > max)
 		return (NULL);
 	arr = malloc(sizeof(int) * size);
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i <= size; i++, min++)
+	for (i = 0; i < size; i++, min++)
 		*(arr + i) = min;
 	return (arr);
 }
