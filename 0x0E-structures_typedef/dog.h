9e9103d8de8f1df9fc1ecd96c2c14142eb66c757
Sinawo <sinaowmngxuma@gmail.com>
2023-03-20 12:25:30 -0700

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+	/**
+	 * @c: This is longer description of C
+	 *
+	 * Description: You can use paragraphs to describe arguments
+	 * using this method.
+	 */
+};
+#endif
