31a49067a27f2820ea5f23167ac456d1425497d0
Sinawo <sinaowmngxuma@gmail.com>
2023-03-21 08:17:35 -0700

    added condtions (i.e if name is NULL or dog is Null)

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,14 @@ void print_dog(struct dog *d)
 
 	if (d != NULL)
 	{
+		if (d->name == NULL)
+			d->name = "(nil)";
+		if (d->owner == NULL)
+			d->owner = "(nil)";
 		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
 	}
+	else
+		printf("(nil)");
 }
