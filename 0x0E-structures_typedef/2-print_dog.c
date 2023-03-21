a05f312d9973a11b89d15d46a82e2477988d9b7c
Sinawo <sinaowmngxuma@gmail.com>
2023-03-21 10:05:02 -0700

    added additional condition to check d->owner

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,7 +1,9 @@
 #include "dog.h"
+#include <string.h>
+#include "_strlen.c"
 #include <stdlib.h>
 #include <stdio.h>
-#include <stddef.h>
+#include "1-init_dog.c"
 /**
  * print_dog - prints dog struct elements
  * @d: pointer to he dog struct in mem
@@ -13,7 +15,7 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 			d->name = "(nil)";
-		if (d->owner == NULL)
+		if (d->owner == NULL || _strlen(d->owner) >= 0)
 			d->owner = "(nil)";
 		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
