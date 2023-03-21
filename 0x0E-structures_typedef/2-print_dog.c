7deca48c19eb9a9555b433405f3f61dd7c423291
Sinawo <sinaowmngxuma@gmail.com>
2023-03-21 10:14:19 -0700

    removed addtional case when d->owner == NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 			d->name = "(nil)";
-		if (d->owner == NULL || _strlen(d->owner) >= 0)
+		if (d->owner == NULL)
 			d->owner = "(nil)";
 		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
